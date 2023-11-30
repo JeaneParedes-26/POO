6d981bfcae386f37223cf7050d928c69b513ac3a
JeaneParedes-26 <150172855+JeaneParedes-26@users.noreply.github.com>
2023-11-29 21:39:13 -0500

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author usuario
+ */
+public class JavaApplication1 {
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        persona per =new persona("Kevin",18);
+        per.correr();
+        // TODO code application logic here
+    }
+    
+}
